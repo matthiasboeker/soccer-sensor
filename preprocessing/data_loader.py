320ed72b356798dfe8ae3b73771ee387e96a04f6
matthiasboeker <matthias.boeker@icloud.com>
2022-02-02 17:47:41 +0100

    initialise players

--- preprocessing/data_loader.py
+++ preprocessing/data_loader.py
@@ -3,14 +3,22 @@ from pathlib import Path
 from dataclasses import dataclass
 from enum import Enum
 
-import pandas as pd
+import pandas as pd  #type: ignore
 
 path_to_input_folder = Path(__file__).parent.parent / "input"
-sheets = ["Fatigue", "Mood", "Readiness", "SleepDurH", "SleepQuality", "Soreness", "Stress"]
+sheets = [
+    "Fatigue",
+    "Mood",
+    "Readiness",
+    "SleepDurH",
+    "SleepQuality",
+    "Soreness",
+    "Stress",
+]
 
 
 class Severity(Enum):
-    Minor = 0,
+    Minor = (0,)
     Major = 1
 
 
@@ -33,18 +41,37 @@ class SoccerPlayer:
     soreness: pd.Series
     stress: pd.Series
 
-def get_player_names(sensor_data):
+
+def get_player_names(sensor_data) -> List[str]:
     return sensor_data["Fatigue"].columns[1:]
 
 
-def initialise_player(sensor_data: Dict[str, pd.DataFrame], player_name: str):
+def get_player_data(sensor_data: Dict[str, pd.DataFrame], player_name: str) -> Dict[str, pd.Series]:
     init = {}
     for attribute, data in sensor_data.items():
-        init[attribute] = data[player_name]
+        init[attribute] = pd.Series(data[player_name]).set_axis(data[f"{attribute} Data"], axis=0)
     return init
 
-def load_in_sheet(path_to_file: Path, sheet_name: str):
-    data_file = pd.read_excel(path_to_file, sheet_name=sheet_name)
+
+def initialise_players(sensor_data: Dict[str, pd.DataFrame]) -> Dict[str, SoccerPlayer]:
+    names = get_player_names(sensor_data)
+    players = {}
+    for id, name in enumerate(names):
+        values = get_player_data(sensor_data, name)
+        players[str(id)] = SoccerPlayer(
+            name,
+            values["Fatigue"],
+            values["Mood"],
+            values["Readiness"],
+            values["SleepDurH"],
+            values["SleepQuality"],
+            values["Soreness"],
+            values["Stress"],
+        )
+    return players
+
+def load_in_sheet(path_to_file: Path, sheet_name: str) -> pd.DataFrame:
+    data_file = pd.read_excel(path_to_file, sheet_name=sheet_name, engine="openpyxl")
     return data_file
 
 
@@ -52,17 +79,5 @@ def load_in_file(path_to_file):
     return {sheet: load_in_sheet(path_to_file, sheet) for sheet in sheets}
 
 
-def load_in_files(path_to_data: List[Path]):
-    print(initialise_player(load_in_file(path_to_data[0]),"Synne Skinnes Hansen"))
-    return [load_in_file(path) for path in path_to_data]
-
-
-def main():
-    path_to_data_2021 = path_to_input_folder / "rosenborg-women_a_2021.xlsx"
-    path_to_data_2020 = path_to_input_folder / "rosenborg-women_a_2020.xlsx"
-    load_in_files([path_to_data_2021, path_to_data_2020])
-
-
-
-if __name__ == "__main__":
-    main()
+def generate_player_data(path_to_data: Path) -> Dict[str, SoccerPlayer]:
+    return initialise_players(load_in_file(path_to_data))
