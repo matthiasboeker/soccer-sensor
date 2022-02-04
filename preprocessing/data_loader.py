3b53163c9eef48824f4511b6d280a16bbd4e9147
matthiasboeker <matthias.boeker@icloud.com>
2022-02-04 11:54:15 +0100

    include injuries

--- preprocessing/data_loader.py
+++ preprocessing/data_loader.py
@@ -1,13 +1,14 @@
-from typing import Any, Dict, List
+from typing import Dict, List, Union
 from pathlib import Path
 from dataclasses import dataclass
-from enum import Enum
 
 import pandas as pd  # type: ignore
 import warnings
 
 path_to_input_folder = Path(__file__).parent.parent / "input"
 sheets = [
+    "Game Performance",
+    "Injury",
     "Fatigue",
     "Mood",
     "Readiness",
@@ -18,16 +19,10 @@ sheets = [
 ]
 
 
-class Severity(Enum):
-    Minor = (0,)
-    Major = 1
-
-
 @dataclass(frozen=True)
 class Injury:
     player: str
-    location: str
-    severity: Severity
+    type: Dict[str, str]
     timestamp: pd.Index
 
 
@@ -41,6 +36,7 @@ class SoccerPlayer:
     sleep_quality: pd.Series
     soreness: pd.Series
     stress: pd.Series
+    injuries: List[Injury]
 
 
 def get_player_names(sensor_data) -> List[str]:
@@ -48,21 +44,40 @@ def get_player_names(sensor_data) -> List[str]:
 
 
 def get_player_data(
-    sensor_data: Dict[str, pd.DataFrame], player_name: str
+    wellness_data: Dict[str, pd.DataFrame], player_name: str
 ) -> Dict[str, pd.Series]:
     init = {}
-    for attribute, data in sensor_data.items():
+    for attribute, data in wellness_data.items():
         init[attribute] = pd.Series(data[player_name]).set_axis(
             data[f"{attribute} Data"], axis=0
         )
     return init
 
 
-def initialise_players(sensor_data: Dict[str, pd.DataFrame]) -> Dict[str, SoccerPlayer]:
-    names = get_player_names(sensor_data)
+def get_player_injuries(
+    player_injuries: pd.DataFrame, player_name: str
+) -> Union[List, List[Injury]]:
+    if not player_injuries.loc[player_name, "Player"].empty():
+        players_injuries = []
+        for injury in player_injuries.loc[player_name, "Player"].iterrows():
+            players_injuries.append(
+                Injury(player_name, injury["Injuries"], injury["Date"])
+            )
+        return players_injuries
+    return []
+
+
+def initialise_players(
+    player_sheets: Dict[str, pd.DataFrame]
+) -> Dict[str, SoccerPlayer]:
+    names = get_player_names(player_sheets)
+    players_injuries = player_sheets["Injury"]
+    del player_sheets["Injury"]
+
     players = {}
     for id, name in enumerate(names):
-        values = get_player_data(sensor_data, name)
+        values = get_player_data(player_sheets, name)
+        injuries = get_player_injuries(players_injuries, name)
         players[str(id)] = SoccerPlayer(
             name,
             values["Fatigue"],
@@ -72,6 +87,7 @@ def initialise_players(sensor_data: Dict[str, pd.DataFrame]) -> Dict[str, Soccer
             values["SleepQuality"],
             values["Soreness"],
             values["Stress"],
+            injuries,
         )
     return players
 
@@ -92,4 +108,6 @@ def load_in_file(path_to_file: List[Path]) -> Dict[str, pd.DataFrame]:
 
 
 def generate_player_data(path_to_data: List[Path]) -> Dict[str, SoccerPlayer]:
-    return initialise_players(load_in_file(path_to_data))
+    data_sheets = load_in_file(path_to_data)
+    player_sheets = [sheet for sheet in sheets if sheet not in ["Game Performance"]]
+    return initialise_players({k: data_sheets[k] for k in player_sheets})
