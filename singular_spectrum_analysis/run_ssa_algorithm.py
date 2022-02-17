6d0a4558346d9e73360baf28dda95cb9ad862237
matthiasboeker <matthias.boeker@icloud.com>
2022-02-17 11:11:51 +0100

    run experiments script

--- singular_spectrum_analysis/run_ssa_algorithm.py
+++ singular_spectrum_analysis/run_ssa_algorithm.py
@@ -1,50 +1,85 @@
 from pathlib import Path
-
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.ticker import MultipleLocator
+import numpy as np  # type: ignore
+import matplotlib.pyplot as plt  # type: ignore
+from matplotlib.ticker import MultipleLocator  # type: ignore
 
 from preprocessing.data_loader import generate_teams
 from singular_spectrum_analysis.ssa_algorithm import SSA, diagonal_averaging
 
-params = {
-    "lag": 31,
-    "order": 10,
-    "threshold": 0.1,
-    "total_iterations": 250
-}
+params = {"lag": 400, "order": 0.995, "threshold": 0.01, "total_iterations": 100}
 
 
 def create_evaluation_plots(real_series, imputed_series):
 
-    fig, axs = plt.subplots(nrows=len(imputed_series), ncols=1, figsize=(15, 10))
+    fig, axs = plt.subplots(
+        nrows=int(np.ceil(len(imputed_series) / 4)), ncols=4, figsize=(50, 25)
+    )
+    for imputed, ax in zip(imputed_series, axs.ravel()):
+        ax.plot(imputed.loss)
+        ax.set_title("Loss")
+        ax.set_xlabel("Iterations")
+        ax.set_ylabel("Loss")
+    plt.savefig(Path(__file__).parent.parent / "evaluation_images" / "loss.png")
 
-    for real_series, imputed_series, ax in zip(real_series, imputed_series, axs.ravel()):
-        ax.plot(np.rint(diagonal_averaging(imputed_series)), label="Rounded Imputed Series")
-        ax.plot(diagonal_averaging(imputed_series), label="Imputed Series")
-        ax.plot(real_series, label="True")
-        ax.title(f"Readiness of a Player with Missing Data")
+    fig, axs = plt.subplots(
+        nrows=int(np.ceil(len(imputed_series) / 4)), ncols=4, figsize=(50, 25)
+    )
+    for imputed, ax in zip(imputed_series, axs.ravel()):
+        ax.plot(imputed.cum_contribution)
+        ax.axvline(x=imputed.rank, linewidth=1, color="red")
+        ax.set_title("Relative Importance")
+        ax.set_xlabel("Dimensions")
+        ax.set_ylabel("Cumulative Contribution (%)")
+    plt.savefig(Path(__file__).parent.parent / "evaluation_images" / "eigen_values.png")
+
+    fig, axs = plt.subplots(nrows=len(imputed_series), ncols=1, figsize=(100, 50))
+    for real, imputed, ax in zip(real_series, imputed_series, axs.ravel()):
+        ax.plot(diagonal_averaging(imputed.trajectory_matrix), label="Imputed Series")
+        ax.plot(real, label="True")
+        ax.set_title("Readiness of a Player with Missing Data")
         ax.tick_params(labelrotation=75, labelsize=9)
         ax.xaxis.set_major_locator(MultipleLocator(30))
         ax.legend()
-        ax.xlabel("Time")
-        ax.ylabel("Readiness Scale")
-        plt.savefig(Path(__file__).parent.parent / "evaluation_images" / "evaluation_plots.png")
+        ax.set_xlabel("Time")
+        ax.set_ylabel("Readiness Scale")
+    plt.savefig(
+        Path(__file__).parent.parent / "evaluation_images" / "evaluation_plots.png"
+    )
 
 
 def main():
 
     path_to_folder = Path(__file__).parent.parent / "input"
-    path_to_data = [[path_to_folder / "rosenborg-women_a_2020.xlsx", path_to_folder / "input" / "rosenborg-women_a_2021.xlsx"],
-                    [path_to_folder / "vifwomen_a_2020.xlsx", path_to_folder / "vifwomen_a_2021.xlsx"]]
+    path_to_data = [
+        [
+            path_to_folder / "rosenborg-women_a_2020.xlsx",
+            path_to_folder / "rosenborg-women_a_2021.xlsx",
+        ],
+        [
+            path_to_folder / "vifwomen_a_2020.xlsx",
+            path_to_folder / "vifwomen_a_2021.xlsx",
+        ],
+    ]
 
     teams = generate_teams(path_to_data, ["VI", "Rosenborg"])
     players = teams["Rosenborg"].players
     print("Players loaded")
 
-    fitted_series = [SSA.fit(player.readiness.to_numpy(), params["lag"], params["order"], params["threshold"],
-                             params["total_iterations"]) for _, player in players.items()]
+    fitted_series = [
+        SSA.fit(
+            player.readiness.to_numpy(),
+            params["lag"],
+            params["order"],
+            params["threshold"],
+            params["total_iterations"],
+        )
+        for _, player in players.items()
+    ]
 
     real_series = [player.readiness.to_numpy() for _, player in players.items()]
 
     create_evaluation_plots(real_series, fitted_series)
+
+
+if __name__ == "__main__":
+    main()
